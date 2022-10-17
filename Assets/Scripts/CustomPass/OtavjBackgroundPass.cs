16d7b2a1a55529e45bf086b2071d6ea2495c0e69
ota <036.tamaki@gmail.com>
2022-10-18 00:36:40 +0900

    add normal custom pass for background

---
+++ Assets/Scripts/CustomPass/OtavjBackgroundPass.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace ota.ndi
+{
+    [System.Serializable]
+    sealed class OtavjBackgroundPass : CustomPass
+    {
+
+        //public RcamBackgroundController _controller = null;
+        public bool _depthOffset = false;
+        public Shader _shader = null;
+
+        Material _material;
+        
+
+        protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
+        {
+            //var shader = Resources.Load<Shader>("OtavjBackground");
+            _material = new Material(_shader);
+            _material.hideFlags = HideFlags.DontSave;
+        }
+
+        protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd,
+           HDCamera hdCamera, CullingResults cullingResult)
+        {
+            //if (_controller == null || !_controller.IsActive) return;
+
+            var extractor = OtavjVFXResource.Extractor;
+            var prj = ProjectionUtils.VectorFromReceiver;
+            var v2w = ProjectionUtils.CameraToWorldMatrix;
+
+            if (extractor == null || extractor.Metadata == null) return;
+
+            _material.SetVector("_ProjectionVector", prj.Value);
+            _material.SetMatrix("_InverseViewMatrix", v2w.Value);
+            _material.SetFloat("_DepthOffset", _depthOffset ? -1e-7f : 0);
+            _material.SetTexture("_ColorTexture", extractor.ColorTexture);
+            _material.SetTexture("_DepthTexture", extractor.DepthTexture);
+
+            //CoreUtils.DrawFullScreen
+            //  (cmd, _material, _controller.PropertyBlock, _controller.PassNumber);
+            CoreUtils.DrawFullScreen(cmd, _material);
+        }
+
+        protected override void Cleanup()
+        {
+            if (Application.isPlaying)
+                Object.Destroy(_material);
+            else
+                Object.DestroyImmediate(_material);
+        }
+    }
+}
+
